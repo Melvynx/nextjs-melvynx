8017ef06aa9b7f53f32636fa9b59c60022bb2e88
melvynxdev <melvynmal@gmail.com>
2025-04-10 12:29:20 +0800

    add prisma and server url

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,18 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const getPrisma = () => {
+  const client = new PrismaClient();
+
+  if (process.env.NODE_ENV === "development") {
+    return client;
+  }
+  return client.$extends(withAccelerate());
+};
+
+export const prisma = globalForPrisma.prisma || getPrisma;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
--- src/lib/server-url.ts
+++ src/lib/server-url.ts
@@ -3,5 +3,13 @@ export const getServerUrl = () => {
     return "http://localhost:3000";
   }
 
+  if (process.env.VERCEL_ENV === "production") {
+    return "https://nextjs-melvynx.vercel.app";
+  }
+
+  if (process.env.VERCEL_ENV === "preview") {
+    return process.env.VERCEL_URL;
+  }
+
   return "https://prod.com";
 };
