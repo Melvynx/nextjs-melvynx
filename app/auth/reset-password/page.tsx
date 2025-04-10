feaa97ec2c9e7be9f71e5e6fcf017c49f0f7d479
melvynxdev <melvynmal@gmail.com>
2025-04-10 12:35:28 +0800

    fff

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,64 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { authClient } from "@/lib/auth-client";
-import { useRouter, useSearchParams } from "next/navigation";
-import { toast } from "sonner";
+import { Suspense } from "react";
+import { ResetPassword } from "./reset-password";
 
 export default function SignUpPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-
-  async function onSubmit(formData: FormData) {
-    const password = formData.get("password") as string;
-
-    if (!token) {
-      toast.error("invalid token");
-      return;
-    }
-
-    await authClient.resetPassword(
-      {
-        newPassword: password,
-        token,
-      },
-      {
-        onSuccess: () => {
-          router.push("/auth/sign");
-          toast.success("Password Reset ! Please sign in.");
-        },
-        onError: (error) => {
-          toast.error(error.error.message);
-        },
-      }
-    );
-  }
-  // 8AWKa8AhxhFfe8QXZlONv8KC
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Reset Password</CardTitle>
-        <CardDescription>{token}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form className="flex flex-col gap-4" action={onSubmit}>
-          <div className="space-y-2">
-            <Label htmlFor="password">New Password</Label>
-            <Input type="password" id="password" name="password"></Input>
-          </div>
-          <Button type="submit">Reset Password</Button>
-        </form>
-      </CardContent>
-    </Card>
+    <Suspense fallback={<p>Loading...</p>}>
+      <ResetPassword />
+    </Suspense>
   );
 }
